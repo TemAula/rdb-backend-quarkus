37a42efefadbd5b36b83cec83aa944a0d4999132
otaviojava <otaviopolianasantana@gmail.com>
2020-11-03 19:45:15 -0300

    test karina

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -11,6 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello Karina";
+        return "hello Karina ---- ";
     }
+    //ignore isso
 }
