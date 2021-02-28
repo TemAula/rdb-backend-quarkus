434eb3a8bbe8dd9ae9a179a9d24fdc9d6a448ffa
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-28 16:06:51 +0000

    atualiza propriedade de producao

--- src/test/java/com/temaula/rdb/security/AdminResourceTest.java
+++ src/test/java/com/temaula/rdb/security/AdminResourceTest.java
@@ -1,6 +1,23 @@
 package com.temaula.rdb.security;
 
-import static org.junit.jupiter.api.Assertions.*;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.Response.Status.FORBIDDEN;
+
+@QuarkusTest
 class AdminResourceTest {
-  
+
+
+    @Test
+    public void shouldReturnForbidden() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/admin")
+                .then()
+                .statusCode(FORBIDDEN.getStatusCode());
+    }
 }
