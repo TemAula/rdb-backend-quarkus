37ffb9deb743375495bf88efa96e880b654150a6
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-28 15:46:04 +0000

    adiciona import do usuario

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+CREATE TABLE usuario (id INT, username VARCHAR(255), password VARCHAR(255), role VARCHAR(255));
+INSERT INTO usuario (id, username, password, role) VALUES (1, 'admin', 'admin', 'admin');
+INSERT INTO usuario (id, username, password, role) VALUES (2, 'user','user', 'user');
