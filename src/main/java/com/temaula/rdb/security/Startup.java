727dff856b79c27acabdaeb8a7fcf2a5a4866f6c
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-28 21:14:05 +0000

    adiciona a logica de iniciar os usuarios

--- src/main/java/com/temaula/rdb/security/Startup.java
+++ src/main/java/com/temaula/rdb/security/Startup.java
@@ -5,12 +5,22 @@ import javax.inject.Singleton;
 import javax.transaction.Transactional;
 
 import io.quarkus.runtime.StartupEvent;
+import org.jboss.logging.Logger;
 
 
 @Singleton
 public class Startup {
+
+    private static final Logger LOGGER = Logger.getLogger(Startup.class);
+
     @Transactional
     public void loadUsers(@Observes StartupEvent evt) {
-
+        LOGGER.info("Iniciando o carregamento dos usuarios");
+        if (Usuario.count() == 0) {
+            LOGGER.info("Nao existe usuarios no banco de dados, iniciando usuarios padroes");
+            Usuario.add("admin", "admin", "admin");
+            Usuario.add("user", "user", "user");
+        }
+        LOGGER.info("Carregamento de usuarios carregado com sucesso");
     }
 }
