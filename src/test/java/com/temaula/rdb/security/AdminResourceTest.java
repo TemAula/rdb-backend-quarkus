2c300860ac2e76f673635669788fa0c99c99a609
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-28 15:59:30 +0000

    adiciona propriedade para carregar usuarios

---
+++ src/test/java/com/temaula/rdb/security/AdminResourceTest.java
@@ -0,0 +1,6 @@
+package com.temaula.rdb.security;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AdminResourceTest {
+  
+}
---
+++ src/test/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario (id, username, password, role) VALUES (1, 'admin', 'admin', 'admin');
+INSERT INTO usuario (id, username, password, role) VALUES (2, 'user','user', 'user');
