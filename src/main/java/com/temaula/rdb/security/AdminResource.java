5bc7d97ffd22cb010b91613afa83cdfef1958a10
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-28 21:15:34 +0000

    remove informacao do usuario

--- src/main/java/com/temaula/rdb/security/AdminResource.java
+++ src/main/java/com/temaula/rdb/security/AdminResource.java
@@ -1,13 +1,10 @@
 package com.temaula.rdb.security;
 
-import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
 
 @Path("/api/admin")
 public class AdminResource {
@@ -19,10 +16,4 @@ public class AdminResource {
         return "admin";
     }
 
-    @GET
-    @PermitAll
-    @Path("/me")
-    public String me(@Context SecurityContext securityContext) {
-        return securityContext.getUserPrincipal().getName();
-    }
 }
